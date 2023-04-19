c516d47c654206ae533338fdd35f83234bea2d3a
JuMiLuGa <lubian.ganete.juliomiguel.ald@gmail.com>
2023-04-19 15:50:34 +0200

    v1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,86 @@
 package com.example;
 
-public class App 
-{
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    private List<Usuario> usuarios;
+
+    public App() {
+        this.usuarios = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("\n********** Menú **********");
+            System.out.println("1. Rexistrar usuario");
+            System.out.println("2. Ver usuarios rexistrados");
+            System.out.println("3. Saír");
+            System.out.print("Escolle unha opción: ");
+
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    app.rexistrarUsuario(scanner);
+                    break;
+                case 2:
+                    app.verUsuariosRexistrados();
+                    break;
+                case 3:
+                    System.out.println("Saíndo do programa...");
+                    break;
+                default:
+                    System.out.println("Opción non válida.");
+                    break;
+            }
+
+        } while (opcion != 3);
+
+        scanner.close();
+    }
+
+    public void rexistrarUsuario(Scanner scanner) {
+        try {
+            System.out.print("\nEmail: ");
+            String email = scanner.next();
+
+            System.out.print("Contrasinal: ");
+            String contrasinal = scanner.next();
+
+            System.out.print("Web: ");
+            String web = scanner.next();
+
+            System.out.print("Data de nacemento (yyyy-MM-dd): ");
+            String nacemento = scanner.next();
+
+            Usuario usuario = new Usuario(email, contrasinal, web, nacemento);
+            usuarios.add(usuario);
+
+            System.out.println("\nUsuario rexistrado con éxito.");
+        } catch (IllegalArgumentException | NoSuchAlgorithmException | ParseException e) {
+            System.out.println("\nErro: " + e.getMessage());
+        }
+    }
+
+    public void verUsuariosRexistrados() {
+        if (usuarios.isEmpty()) {
+            System.out.println("\nNon hai usuarios rexistrados.");
+        } else {
+            System.out.println("\nUsuarios rexistrados:");
+            for (Usuario usuario : usuarios) {
+                System.out.println("- " + usuario.getEmail());
+            }
+        }
     }
 }
+
---
+++ src/main/java/com/example/Validador.java
@@ -0,0 +1,53 @@
+package com.example;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validador {
+    public static boolean validarEmail(String email) {
+        String regex = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static boolean validarContrasinal(String contrasinal) {
+        if (contrasinal.length() < 8) {
+            return false;
+        }
+
+        String regex = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(contrasinal);
+        return matcher.matches();
+    }
+
+    public static String hashearContrasinal(String contrasinal) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(contrasinal.getBytes());
+            byte[] digest = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
+
+    public static boolean validarWeb(String web) {
+        String regex = "^(http(s)?://)?([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?$";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(web);
+        return matcher.matches();
+    }
+
+    public static boolean validarNacemento(LocalDate nacemento) {
+        return nacemento.isBefore(LocalDate.now());
+    }
+}
